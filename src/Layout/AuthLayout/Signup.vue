11fcbcdef2f1cd520234885493ab27cdf5058d6c
Elite <tretyak.sanya1@gmail.com>
2022-01-14 07:17:02 -0800

    MINI-02: Complete Login Component

---
+++ src/Layout/AuthLayout/Signup.vue
@@ -0,0 +1,36 @@
+<template>
+  <MDBCard>
+    <div>
+      <img
+        src="../../assets/logo.png"
+        alt="..."
+        class="img"
+      />
+    </div>
+
+    <MDBCardBody>
+      <MDBCardTitle>Login</MDBCardTitle>
+      <MDBInput label="Email" class="mb-3"/>
+      <MDBInput label="Password" class="mb-3"/>
+      <MDBInput label="Confirm Password" class="mb-3"/>
+      <div class="d-flex justify-content-around">
+        <MDBBtn color="info">Signup</MDBBtn>
+        <MDBBtn color="primary">Login</MDBBtn>
+      </div>
+    </MDBCardBody>
+  </MDBCard>
+</template>
+
+<script>
+  import { MDBCard, MDBCardBody, MDBCardTitle, MDBInput, MDBBtn } from 'mdb-vue-ui-kit';
+
+  export default {
+    name: "Signup",
+    components: {
+      MDBCard, MDBCardBody, MDBCardTitle, MDBInput, MDBBtn
+    }
+  }
+</script>
+
+<style scoped>
+</style>
